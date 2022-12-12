0df8ba6f9249225394e96e315063544040e452e2
DCET2Brent <120375954+DCET2Brent@users.noreply.github.com>
2022-12-12 23:45:32 +0800

    Add files via upload

---
+++ Brutas_RonnelBrent(2inputs).cpp
@@ -0,0 +1,25 @@
+/*The same function lang po ito sir, pero dito it just only need to input 2 values to solve unlike sa another code na ipinasa ko po, there's 3 main inputs and additional 1 input for continuation if ever want uli mag solve ng another problem po*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+     void();{
+     cout <<"Ronnel Brent D. Brutas" <<endl;
+     cout <<"DCET 2-1" <<endl;
+     cout <<"\n<<<<<SIMPLE CALCULATOR>>>>>"<<endl;
+}
+     float x, y, add, sub, mul, div;
+     cout << "\nEnter first value: " <<endl;
+     cin >> x;
+     cout << "Enter second value: " <<endl;
+     cin >> y;
+     add=x+y;
+     cout << "\nSum: " <<add;
+     sub=x+y;
+     cout << "\nDifference: "<<sub;
+     mul=x*y;
+     cout << "\nProduct: "<<mul;
+     div=x/y;
+     cout << "\nQuotient: "<<div;
+}
